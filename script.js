b776178336fc8306e7a583a0b197f8c49669d952
Jasper <halvorsonjasper11@gmail.com>
2024-06-25 15:40:20 -0700

    simple home page

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-document.getElementById('navigateButton').addEventListener('click', function() {
+const buttons = document.querySelectorAll('.navigateButton');
+
+buttons.forEach(button => {
+    button.addEventListener('click', function() {
+        const section = this.getAttribute('data-section');
+        alert(`Navigating to ${section}`);
+        // Here you can add the logic to navigate or show different sections
+    });
+});
+
+document.getElementById('navigateDeeper').addEventListener('click', function() {
     window.location.href = 'depth2.html';
 });
